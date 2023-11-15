e068ca79ae62256f83b7fd2958cb6907fe7514aa
abdih <abdihashi2001@hotmail.com>
2023-11-15 18:52:24 -0500

    head function

---
+++ head.js
@@ -0,0 +1,46 @@
+console.assert(1 === 1);      
+console.assert(1 === 1.1);   
+
+
+const sum = function(a, b) { 
+  return a + b;
+}
+
+
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3); 
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const head = function(arr) {
+  
+  if (arr.length > 0) {
+    
+    return arr[0];
+  } else {
+    
+    return undefined;
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp"); 
+assertEqual(1, 1);  
+
+
+
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("hello", "hello"); 
+assertEqual("world", "world!");
