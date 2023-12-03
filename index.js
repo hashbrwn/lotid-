3ab2fe1fac272df42059d609bc131f9c7b83df57
hashbrwn <abdihashi2001@hotmail.com>
2023-12-03 12:01:11 -0500

    Export Lotide functions in index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+// Add references to other utility functions here
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual
+  // Add other utility functions here
+};
