2aa3d8dc85ccfe701ab421735df5366772960894
hashbrwn <abdihashi2001@hotmail.com>
2023-12-03 08:37:15 -0500

    Implement findKey function and add test cases

---
+++ findKey.js
@@ -0,0 +1,34 @@
+// findKey.js
+
+const assertEqual = function(value1, value2) {
+  if (value1 === value2) {
+    console.log(`✅✅✅ Assertion Passed: ${value1} === ${value2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${value1} !== ${value2}`);
+  }
+};
+
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    
+    
+    if (callback(object[key])) 
+    
+    
+    return key;
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+
+// Export the findKey function
+module.exports = findKey;
